ecf9eeb616ac1e2e1356206e9e37f59830f9a1a2
Eugene <git@telegrafo.ru>
2023-09-09 02:48:14 +0500

    Add `esl/gradient`

--- mix.exs
+++ mix.exs
@@ -39,6 +39,7 @@ defmodule UrFUSwissBot.MixProject do
       # Code quality
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.3", only: [:dev, :test], runtime: false},
+      {:gradient, github: "esl/gradient", only: [:dev, :test], runtime: false},
       # Secret
       {:secret_vault, "~> 1.0"}
     ]
