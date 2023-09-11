008b4b979564376dce9883d4f3a7ffd550c3b4f6
Eugene <git@telegrafo.ru>
2023-09-11 14:41:18 +0500

    Add types to `Repo`

--- lib/urfu_swiss_bot/repo.ex
+++ lib/urfu_swiss_bot/repo.ex
@@ -1,8 +1,12 @@
 defmodule UrFUSwissBot.Repo do
   @repo __MODULE__
 
+  @spec put(CubDB.key(), CubDB.value()) :: :ok
   def put(key, value), do: CubDB.put(@repo, key, value)
-  @spec get(any, any) :: any
+
+  @spec get(CubDB.key(), CubDB.value()) :: CubDB.value()
   def get(key, default \\ nil), do: CubDB.get(@repo, key, default)
+
+  @spec delete(CubDB.key()) :: :ok
   def delete(key), do: CubDB.delete(@repo, key)
 end
