7e1a2770a4eb2cb09717513adacd699e2393db34
Eugene <git@telegrafo.ru>
2023-09-13 21:30:37 +0500

    Add `Repo.select` and `Repo.transaction`

--- lib/urfu_swiss_bot/repo.ex
+++ lib/urfu_swiss_bot/repo.ex
@@ -7,6 +7,12 @@ defmodule UrFUSwissBot.Repo do
   @spec get(CubDB.key(), CubDB.value()) :: CubDB.value()
   def get(key, default \\ nil), do: CubDB.get(@repo, key, default)
 
+  @spec select([CubDB.select_option()]) :: Enumerable.t()
+  def select(options \\ []), do: CubDB.select(@repo, options)
+
   @spec delete(CubDB.key()) :: :ok
   def delete(key), do: CubDB.delete(@repo, key)
+
+  @spec transaction(function) :: any
+  def transaction(fun), do: CubDB.transaction(@repo, fun)
 end
