0982135be8ba87ca5f542008cbed8e0d67d741dc
Eugene <git@telegrafo.ru>
2023-10-08 01:57:39 +0500

    Filter non-authed users

--- lib/urfu_swiss_knife/cache_warming.ex
+++ lib/urfu_swiss_knife/cache_warming.ex
@@ -11,9 +11,7 @@ defmodule UrFUSwissKnife.CacheWarming do
 
   @spec warm_today_schedule :: :ok
   def warm_today_schedule do
-    for user <- Accounts.get_users() do
-      {:ok, auth} = Modeus.auth_user(user)
-
+    for {_user, auth} <- get_authed_users(Modeus) do
       today =
         DateTime.utc_now()
         |> Utils.start_of_day()
@@ -27,9 +25,7 @@ defmodule UrFUSwissKnife.CacheWarming do
 
   @spec warm_ubu_dates :: :ok
   def warm_ubu_dates do
-    for user <- Accounts.get_users() do
-      {:ok, auth} = UBU.auth_user(user)
-
+    for {user, auth} <- get_authed_users(UBU) do
       was = Cache.get({:get_dates, user.username})
       became = UBU.update_dates_cache(auth)
 
@@ -43,12 +39,17 @@ defmodule UrFUSwissKnife.CacheWarming do
 
   @spec warm_istudent_brs :: :ok
   def warm_istudent_brs do
-    for user <- Accounts.get_users() do
-      {:ok, auth} = IStudent.auth_user(user)
-
+    for {_user, auth} <- get_authed_users(IStudent) do
       IStudent.update_subjects_cache(auth)
     end
 
     :ok
+  end
+
+  defp get_authed_users(auth_module) do
+    Accounts.get_users()
+    |> Stream.map(fn user -> {user, auth_module.auth_user(user)} end)
+    |> Stream.filter(&match?({:ok, _auth}, elem(&1, 1)))
+    |> Stream.map(fn {user, {:ok, auth}} -> {user, auth} end)
   end
 end
