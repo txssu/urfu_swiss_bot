5e4d538410a8ec4faf4783edab85da858bbc1bfa
Eugene <git@telegrafo.ru>
2023-10-08 01:34:21 +0500

    Remove consolidating warnings in dev and test

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule UrFUSwissBot.MixProject do
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
+      consolidate_protocols: Mix.env() == :prod,
       deps: deps()
     ]
   end
