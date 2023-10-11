658025489c4b3b26b2ee07f4288362131e4566e2
Eugene <git@telegrafo.ru>
2023-10-12 02:12:30 +0500

    Change UBU dates warming hours

--- config/config.exs
+++ config/config.exs
@@ -17,8 +17,8 @@ config :urfu_swiss_knife, UrFUSwissKnife.Scheduler,
     {"@reboot", {UrFUSwissKnife.CacheWarming, :warm_all, []}},
     # At 00:00 on every day-of-week from Monday through Saturday.
     {"0 0 * * 1-6", {UrFUSwissKnife.CacheWarming, :warm_today_schedule, []}},
-    # At 14:00.
-    {"0 14 * * *", {UrFUSwissKnife.CacheWarming, :warm_ubu_dates, []}},
+    # At minute 0 past every 4th hour from 8 through 20.
+    {"0 8-20/4 * * *", {UrFUSwissKnife.CacheWarming, :warm_ubu_dates, []}},
     # At every 15th minute.
     {"*/15 * * * *", {UrFUSwissKnife.CacheWarming, :warm_istudent_brs, []}}
   ]
