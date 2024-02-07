11d278e6a5ce5bb7dddc17874787c1af005bb394
Eugene <git@telegrafo.ru>
2024-02-08 02:09:12 +0500

    chore: remove shell.nix, use flake.nix

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs = { url = "github:NixOS/nixpkgs/nixos-23.11"; };
+    flake-utils = { url = "github:numtide/flake-utils"; };
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        inherit (pkgs.lib) optional optionals;
+        pkgs = import nixpkgs { inherit system; };
+
+        beamPkg = pkgs.beam.packagesWith pkgs.erlangR26;
+
+        elixir = beamPkg.elixir.override {
+          version = "1.16.1";
+          sha256 = "rjUt3gCUszCbzGE7BriwH3ptrV81dqNB/d0nVOXrcGI=";
+        };
+      in
+      with pkgs;
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            elixir
+            elixir_ls
+            glibcLocales
+          ] ++ optional stdenv.isLinux inotify-tools
+          ++ optional stdenv.isDarwin terminal-notifier
+          ++ optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
+            CoreFoundation
+            CoreServices
+          ]);
+        };
+      });
+}
